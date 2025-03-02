c28fb816d3269c89490ed6235717391e83be3dc3
tmaandi <tmaandi@umich.edu>
2025-03-01 21:17:13 -0700

    Created ABS, renames lib to SC - System Common

---
+++ ABS/abs.c
@@ -0,0 +1,9 @@
+
+/*
+
+Problem: “Implement an ABS state machine with states Idle, 
+Apply, Release. Use pidControl to adjust pressure based on filtered speed. 
+Transition: Idle if speed ≈ setpoint, Apply if speed < setpoint, 
+Release if speed > setpoint.”
+
+*/
